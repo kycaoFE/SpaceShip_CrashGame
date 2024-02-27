30c7b1b5a31e309e0f5239f34ab2b7ff031cacf6
kycaoFE <ky.cao@enotion.io>
2024-02-27 18:14:46 +0700

    feat change skin Ship (In progress)

--- 2_Scripts/Controller/containerShipController.ts
+++ 2_Scripts/Controller/containerShipController.ts
@@ -1,4 +1,4 @@
-import { Vec3 } from 'cc';
+import { Vec3, SpriteFrame} from 'cc';
 import { tween } from 'cc';
 import { Sprite } from 'cc';
 import { Vec2 } from 'cc';
@@ -19,15 +19,14 @@ export class containerShipController extends Component {
     private locationDeltaMouse: Vec2;
     private isMoving: boolean;
     private _shipController: any;
+    private numShipCurrent: number;
 
 
     start() {
         this._shipController = this.shipController.getComponent('ShipController')
         this.locationDeltaMouse = new Vec2(0,0);
         this.isMoving = false;
-        this.spriteArray.push(this.shipLeft);
-        this.spriteArray.push(this.shipMid);
-        this.spriteArray.push(this.shipRight);
+        this.numShipCurrent = 1;
 
         this._shipController.setShipIdle(this.shipMid.spriteFrame);
         this.node.on(Node.EventType.TOUCH_MOVE, (event) => {
@@ -41,13 +40,13 @@ export class containerShipController extends Component {
         }, this);
 
         this.node.on(Node.EventType.TOUCH_CANCEL, (event) => {
-            this.isMoving = false;
-            // this.checkInMid();
+            // this.isMoving = false;
+            // // this.checkInMid();
         }, this);
     }
 
     update(deltaTime: number) {
-
+        if(this.node.position.x < 5 && this.node.position.x > -5) return;
         if(this.isMoving) {
             this.spriteArray.forEach(element => {
                 const scaleX = 1.5-(Math.abs((this.node.position.x+ element.node.position.x)*2/1000))
@@ -60,42 +59,52 @@ export class containerShipController extends Component {
 
     moveContainer(event){
         this.locationDeltaMouse = event.getUIDelta();
-        console.log(this.locationDeltaMouse);
         const newPosition = this.node.getPosition(); 
         this.node.setPosition(new Vec3(newPosition.x + this.locationDeltaMouse.x));
     }
 
     checkInMid(){
-        if(this.node.position.x > 0){
+        console.log('check');
+        if(this.node.position.x > 5){
             this.isMoving = true;
             tween(this.node)
             .to(0.5, {position: new Vec3(600,0,0)})
             .call(()=>{
-                const tempSprite = this.shipRight.spriteFrame;
-                this.shipRight.spriteFrame = this.shipMid.spriteFrame;
-                this.shipMid.spriteFrame = this.shipLeft.spriteFrame;
-                this.shipLeft.spriteFrame = tempSprite;
+                const tempSprite = this.spriteArray[this.spriteArray.length - 1].spriteFrame;
+                for(let i = this.spriteArray.length-1; i >= 0; i--){
+                    if(i > 0){
+                        this.spriteArray[i].spriteFrame = this.spriteArray[i - 1].spriteFrame;
+                    }
+                    else{
+                        this.spriteArray[i].spriteFrame = tempSprite;
+                    }
+                }
                 this.node.position = new Vec3(0,0,0);
-                this.shipMid.node.scale = new Vec3(1.5, 1.5, 1.5);
+                this.spriteArray[1].node.scale = new Vec3(1.5, 1.5, 1.5);
                 this.isMoving = false;
-                this._shipController.setShipIdle(this.shipMid.spriteFrame);
+                this._shipController.setShipIdle(this.spriteArray[1].spriteFrame);
                 return true;
             })
             .start()
         }
-        if(this.node.position.x < 0){
+        if(this.node.position.x < -5){
             this.isMoving = true;
             tween(this.node)
             .to(0.5, {position: new Vec3(-600,0,0)})
             .call(()=>{
-                const tempSprite = this.shipLeft.spriteFrame;
-                this.shipLeft.spriteFrame = this.shipMid.spriteFrame;
-                this.shipMid.spriteFrame = this.shipRight.spriteFrame;
-                this.shipRight.spriteFrame = tempSprite;
+                const tempSprite = this.spriteArray[0].spriteFrame;
+                for(let i = 0; i < this.spriteArray.length; i++){
+                    if(i < this.spriteArray.length - 1){
+                        this.spriteArray[i].spriteFrame = this.spriteArray[i + 1].spriteFrame;
+                    }
+                    else{
+                        this.spriteArray[i].spriteFrame = tempSprite;
+                    }
+                }
                 this.node.position = new Vec3(0,0,0);
-                this.shipMid.node.scale = new Vec3(1.5, 1.5, 1.5);
+                this.spriteArray[1].node.scale = new Vec3(1.5, 1.5, 1.5);
                 this.isMoving = false;
-                this._shipController.setShipIdle(this.shipMid.spriteFrame);
+                this._shipController.setShipIdle(this.spriteArray[1].spriteFrame);
                 return true;
             })
             .start()
